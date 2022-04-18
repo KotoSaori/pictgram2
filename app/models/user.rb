7f58ec761b3c3af663759ab7bdc1f1a5b1d7595d
EC2 Default User <ec2-user@ip-172-31-44-69.us-east-2.compute.internal>
2022-04-18 23:47:54 +0000

    20220419 commit for kadai rails04

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 15 }
+    validates :email, presence: true, format: { with: /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/ }
+
+    validates :password, length: { in: 8..32 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    has_secure_password
+    
+end
